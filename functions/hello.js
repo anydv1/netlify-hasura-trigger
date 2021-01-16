45e67e5d4fea715a54b58a015d2ba2f302117c79
anupama.yadav <anupama.yadav@doma.co.in>
2021-01-16 11:11:29 +0530

    update

--- functions/hello.js
+++ functions/hello.js
@@ -34,24 +34,17 @@ exports.handler = async(event, context, cb) => {
   //const { event: {op, data}, table: {name, schema} } = event;
   
   let request;
-  let {created_by,created_at,modified_at,modified_by,deleted,properties,id,log_remarks,start_date,end_date}=data.new;
+  let {cron_expression,id}=data.new;
   let payload={
-    created_by,
-    created_at,
-    modified_at,
-    modified_by,
-    deleted,
-    properties,
-    sub_subscription_id:id,
-    log_remarks,
-    
+    cron_expression:cron_expression,
+    id:id
   }
  
   if(op === 'INSERT'){
-    const qv ={id:data.new.sub_package_id};
+    const cron_expre ={id:data.new.cron_expression};
     let data1 = JSON.stringify({
-      query: sub_package_query,
-      variables: qv
+      // query: sub_package_query,
+      variables: cron_expre
     });
     
     let config = {
@@ -67,32 +60,75 @@ exports.handler = async(event, context, cb) => {
    const response=await axios(config);
    const res=response.data.data;
   console.log(res);
-    
 
- const service_id=res.vas_sub_packages[0].services[0].service_id;
- const duration=res.vas_sub_packages[0].duration;
- payload.service_id=service_id;
+  const options = {
+    uri: "https://server.internal.multiliving.co.in/v1/query",
+    headers: {
+            "X-Hasura-Role": "admin",
+            "x-hasura-admin-secret":process.env.HASURA_ADMIN_SECRET
+    },
+    body:{
+      "type": "create_cron_trigger",
+      "args": {
+         "name": "eod_reports",
+         "webhook": "https://server.internal.multiliving.co.in:8081/gql/api/generic/daily_reports",
+         "schedule": "0 22 * * 1-5",
+         "include_in_metadata": true,
+         "payload": {},
+         "retry_conf": {
+               "num_retries": 3,
+               "timeout_seconds": 120,
+               "tolerance_seconds": 21675,
+               "retry_interval_seconds": 12
+         },
+         "comment": "sample_cron commment"
+      }
+    },
+    json: true
+};
 
-    const freq=duration.split(":");
-    let frequency;
-    if(freq[0] == '01') frequency='M';
-    else if(freq[1] == '01') frequency='w';
-    else frequency='d';   
-    const final_payload=[];
-    while(moment(start_date).isSameOrBefore(end_date)){
-      final_payload.push({...payload,service_date:start_date});
-      start_date=moment(start_date, "YYYY-MM-DD").add(1, frequency).format("YYYY-MM-DD");
-    }
-    console.log("final payload ", final_payload);
-   
-    let insert_data = JSON.stringify({
-      query:insert_tracker,
-      variables: {objects:final_payload}
-    });
-    config.data=insert_data;
-   const track= await axios(config);
-    console.log(track.data);
+  // const create_cron={
+  //    "type": "create_cron_trigger",
+  //    "args": {
+  //       "name": "eod_reports",
+  //       "webhook": "https://server.internal.multiliving.co.in:8081/gql/api/generic/daily_reports",
+  //       "schedule": "0 22 * * 1-5",
+  //       "include_in_metadata": true,
+  //       "payload": {},
+  //       "retry_conf": {
+  //             "num_retries": 3,
+  //             "timeout_seconds": 120,
+  //             "tolerance_seconds": 21675,
+  //             "retry_interval_seconds": 12
+  //       },
+  //       "comment": "sample_cron commment"
+  //    }
+  // }
+
+  // const service_id=res.vas_sub_packages[0].services[0].service_id;
+  // const duration=res.vas_sub_packages[0].duration;
+  // payload.service_id=service_id;
+ 
+  //    const freq=duration.split(":");
+  //    let frequency;
+  //    if(freq[0] == '01') frequency='M';
+  //    else if(freq[1] == '01') frequency='w';
+  //    else frequency='d';   
+  //    const final_payload=[];
+  //    while(moment(start_date).isSameOrBefore(end_date)){
+  //      final_payload.push({...payload,service_date:start_date});
+  //      start_date=moment(start_date, "YYYY-MM-DD").add(1, frequency).format("YYYY-MM-DD");
+  //    }
+  //    console.log("final payload ", final_payload);
     
+  //    let insert_data = JSON.stringify({
+  //      query:insert_tracker,
+  //      variables: {objects:final_payload}
+  //    });
+  //    config.data=insert_data;
+  //   const track= await axios(config);
+  //    console.log(track.data);
+
    
   }
   // else if(op=== 'UPDATE'){
