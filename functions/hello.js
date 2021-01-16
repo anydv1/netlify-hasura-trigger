d938b079a381650377643fce4ffa0271def9d4fb
anupama.yadav <anupama.yadav@doma.co.in>
2021-01-16 12:19:43 +0530

    update1

--- functions/hello.js
+++ functions/hello.js
@@ -3,34 +3,10 @@ const axios = require('axios');
 const moment=require('moment');
 const rp = require('request-promise')
 
-const sub_package_query=`query vas_sub_packages($id: uuid){
-  vas_sub_packages(where:{id:{_eq:$id}}){
-    id
-    duration
-    services{
-      service_id
-    }
-  }
-}`
-const insert_tracker=`mutation insertTracker($objects: [vas_subscription_tracker_insert_input!]! ) {
-  insert_vas_subscription_tracker(objects: $objects) {
-    returning {
-      id
-    }
-  }
-}`
-const update_tracker=`mutation updateTracker($object: vas_subscription_tracker_set_input,$id:uuid!){
-  update_vas_subscription_tracker(where:{sub_subscription_id:{_eq:$id}},_set:$object){
-    affected_rows
-    returning{
-      id
-    }
-  }
-}`;
+
 
 exports.handler = async(event, context, cb) => {
-  const adminSecret ="8f70264534ccb260579b8a658601141a";
-  const hgeEndpoint = "http://server.internal.multiliving.co.in/v1/graphql";
+ 
   try{
     const { event: {op, data} } = JSON.parse(event.body);
   //const { event: {op, data}, table: {name, schema} } = event;
@@ -39,8 +15,7 @@ exports.handler = async(event, context, cb) => {
   
  
   if(op === 'INSERT'){
-  
-    
+    console.log('called')
     const options = {
       uri: "https://server.internal.multiliving.co.in/v1/query",
       headers: {
@@ -61,15 +36,15 @@ exports.handler = async(event, context, cb) => {
   };
   const responses=await rp.post(options)
   console.log(responses,'here')
-  return responses;
+  // return responses;
    
   }
-
-
   cb(null, {
     statusCode: 200,
     body: "success"
   });
+
+
 }
 catch(err){
   console.log(err,'error');
